2725f16ba11ba0b769e8d5c0ef836d047da25c06
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 10:39:52 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,6 +5,6 @@ define('BASEURL' , 'http://localhost/travel/public');
 //database
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '1234');
+define('DB_PASS', "");
 define('DB_NAME', 'travel');
 ?>
