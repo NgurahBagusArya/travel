a1f9e67a209c92f49804464e0e9aa3272354614e
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 00:17:29 +0800

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,23 +10,3 @@
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/style-login.css">
 </head>
   <body>
-    <nav class="navbar">
-      <div class="content">
-        <div class="logo">
-          <a href="<?= BASEURL ?>">Travel Kuy</a>
-        </div>
-        <ul class="menu-list">
-          <div class="icon cancel-btn">
-            <i class="fas fa-times"></i>
-          </div>
-          <li><a href="<?= BASEURL ?>">Home</a></li>
-          <li><a href="<?= BASEURL ?>/About">About</a></li>
-          <li><a href="<?= BASEURL ?>">Services</a></li>
-          <li><a href="<?= BASEURL ?>">Features</a></li>
-          <li><a href="<?= BASEURL ?>">Contact</a></li>
-        </ul>
-        <div class="icon menu-btn">
-          <i class="fas fa-bars"></i>
-        </div>
-      </div>
-    </nav>
