7e44f4eba0d5be792ec9e9a1ea7f2f14eacdffe6
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 14:02:03 +0800

    Update registrasi.php

--- app/views/login/registrasi.php
+++ app/views/login/registrasi.php
@@ -4,22 +4,22 @@
     <!-- FORM REGISTER -->
     <div class="form-group">
         <label for="username" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" placeholder="username" name="username">
+        <input type="text" class="form-control" id="username" placeholder="Username" name="username" required>
     </div>
 
     <div class="form-group">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" placeholder="email" name="email">
+        <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
     </div>
 
     <div class="form-group">
         <label for="no_telp" class="form-label">Nomer Telephone</label>
-        <input type="number" class="form-control" id="no_telp" placeholder="no_telp" name="no_telp">
+        <input type="number" class="form-control" id="no_telp" placeholder="Nomer Telepon" name="no_telp" required>
     </div>
 
     <div class="form-group">
         <label for="password" class="form-label">password</label>
-        <input type="password" class="form-control" id="password" placeholder="password" name="password">
+        <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
     </div>
     </div>
     <div class="modal-footer">
