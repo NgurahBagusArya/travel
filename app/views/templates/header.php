9cf3a4dd8a073abc1a9c7a33bf59bb10fa0cc9b4
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 10:39:54 +0800

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> <?= $data['judul']; ?></title>
-  <link rel="stylesheet" href="<?= BASEURL; ?> /css/bootstrap/bootstrap.css">
+  <link rel="stylesheet" href="<?= BASEURL; ?> /css/bootstrap.css">
 </head>
 
 <body>
