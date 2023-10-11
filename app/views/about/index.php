e2df51b6bbbabade90480640de690cac85e8beb7
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 14:02:00 +0800

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,10 @@
-
+<div class="banner">
+    <img src="img/main.jpg" alt="" width="100%">
+</div>
+    <div class="about">
     <h1>Index (About)</h1>
     <p>Halo, I Am Leo I Am Gamer</p>
     <p>Halo, i Am <?= $data['nama'];  ?> i Am <?= $data['pekerjaan']; ?></p>
+    </div>
+
+  
