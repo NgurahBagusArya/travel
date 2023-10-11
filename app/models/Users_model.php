6379a4aa354ab581e7d47eb0d0b960a415c86e19
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 11:21:46 +0800

    Update Users_model.php

--- app/models/Users_model.php
+++ app/models/Users_model.php
@@ -21,21 +21,21 @@ class Users_Model{
 
      public function tambahDataUser($data)
      {
-        $query =  "INSERT INTO travel
-                    VALUES
-                    ('', :username, :email, :no_telp, :password, :level)";
+        $query = "INSERT INTO user (username, email, no_telp, password, level)
+        VALUES (:username, :email, :no_telp, :password, :level)";
 
-                    $this->db->query($query);
-                    $this->db->bind('username', $data['username']);
-                    $this->db->bind('email', $data['email']);
-                    $this->db->bind('no_telp', $data['no_telp']);
-                    $this->db->bind('password', $data['password']);
-                    $data['level'] = "user";
-                    $this->db->bind('level', $data['level']);
+        $this->db->query($query);
+        $this->db->bind('username', $data['username']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('no_telp', $data['no_telp']);
+        $this->db->bind('password', $data['password']);
+        $data['level'] = "user";
+        $this->db->bind('level', $data['level']);
 
-                    $this->db->execute();
+        $this->db->execute();
 
-                    return $this->db->rowCount();
+        return $this->db->rowCount();
+
      }
 
 
