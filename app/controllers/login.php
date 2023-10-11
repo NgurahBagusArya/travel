7222892f56cf10fe7b8a99c75b7187b9105d2bc0
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 22:11:49 +0800

    Update login.php

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,18 +1,54 @@
 <?php
+class Login extends Controller {
+    public function index() {
+        if (isset($_POST['login'])) {
+            $this->prosesLogin();
+        } else {
+            $data['judul'] = 'Login';
+            $this->view('templates/header', $data);
+            $this->view('login/login');
+            $this->view('templates/footer');
+        }
+    }
 
-class Login extends Controller{
-    public function index()
-    {
-        $data['judul'] = 'Daftar Users';
-        $data['mhs'] = $this->model('Users_model')->getAllUsers();
+    private function prosesLogin() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $userModel = $this->model('Users_Model');
+        $user = $userModel->getUserByUsername($username);
+
+        if ($user) {
+            // Verifikasi kata sandi
+            if (password_verify($password, $user['password'])) {
+                // Berhasil login
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                header('Location: ' . BASEURL . '/home');
+                exit;
+            } else {
+                // Password salah
+                $pesan = 'Password salah.';
+                $this->tampilkanPesanError($pesan);
+            }
+        } else {
+            // Username tidak ditemukan
+            $pesan = 'Username tidak ditemukan.';
+            $this->tampilkanPesanError($pesan);
+        }
+    }
+
+    private function tampilkanPesanError($pesan) {
+        $data['pesan'] = $pesan;
+        $data['judul'] = 'Login';
         $this->view('templates/header', $data);
-        $this->view('login/login');
+        $this->view('login/login', $data);
         $this->view('templates/footer');
     }
 
     public function registrasi()
     {
-        $data['judul'] = 'Detail Users';
+        $data['judul'] = 'Registrasi';
         $this->view('templates/header', $data);
         $this->view('login/registrasi');
         $this->view('templates/footer');
@@ -20,8 +56,8 @@ class Login extends Controller{
 
     public function tambah()
     {
-        if( $this->model('Users_model')->tambahDataUser($_POST) > 0){
-            header('Location:' . BASEURL .'/login');
+        if ($this->model('Users_Model')->tambahDataUser($_POST) > 0) {
+            header('Location:' . BASEURL . '/login');
             exit;
         }
     }
