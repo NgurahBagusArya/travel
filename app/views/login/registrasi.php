fc4a7608a6a067fa93e23dc4a0bc75d53503f55d
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 10:46:59 +0800

    Update registrasi.php

--- app/views/login/registrasi.php
+++ app/views/login/registrasi.php
@@ -8,11 +8,9 @@
     </div>
 
     </div>
-
     <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">close</button>
         <button type="button" class="btn btn-primary">Tambah data</button>
 
     </div>
-
 </form>
