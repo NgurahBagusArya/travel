54f11625bce97c0b548df6e4a4887f027cb23ecf
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 00:17:30 +0800

    Update navbar.php

---
+++ app/views/templates/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar">
+      <div class="content">
+        <div class="logo">
+          <a href="<?= BASEURL ?>">Travel Kuy</a>
+        </div>
+        <ul class="menu-list">
+          <div class="icon cancel-btn">
+            <i class="fas fa-times"></i>
+          </div>
+          <li><a href="<?= BASEURL ?>">Home</a></li>
+          <li><a href="<?= BASEURL ?>/About">About</a></li>
+          <li><a href="<?= BASEURL ?>">Services</a></li>
+          <li><a href="<?= BASEURL ?>">Contact</a></li>
+          <li><a href="<?= BASEURL ?>/login">Login</a></li>
+        </ul>
+        <div class="icon menu-btn">
+          <i class="fas fa-bars"></i>
+        </div>
+      </div>
+    </nav>
