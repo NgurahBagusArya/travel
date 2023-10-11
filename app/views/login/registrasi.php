2171433265094219c403296ef8089c7e014e2bb9
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-11 22:52:38 +0800

    Update registrasi.php

--- app/views/login/registrasi.php
+++ app/views/login/registrasi.php
@@ -1,29 +1,37 @@
-<div class="modal-body"></div>
+    <div class="container">
+        <section>
+            <div class="imgBx">
+                <img src="<?= BASEURL ?>\img\background-register" alt="Background Image">
+            </div>
+            <div class="contentBx">
+                <div class="formBx">
+                    <h2>Register</h2>
+                    <p>Welcome To TravelKuy</p>
+                    <form action="<?= BASEURL ?>/login/tambah" method="post">
+                        <div class="inputBx">
+                            <span></span>
+                            <input type="text" class="form-control" id="username" placeholder="Username" name="username" style="background: transparent;" required>
+                        </div>
+                        <div class="inputBx">
+                            <span></span>
+                            <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
+                        </div>
+                        <div class="inputBx">
+                            <span></span>
+                            <input type="number" class="form-control" id="no_telp" placeholder="Nomer Telepon" name="no_telp" required>
+                        </div>
+                        <div class="inputBx">
+                            <span></span>
+                            <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+                        </div>
 
-<form action="<?= BASEURL ?>/login/tambah" method="post">
-    <!-- FORM REGISTER -->
-    <div class="form-group">
-        <label for="username" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" placeholder="Username" name="username" required>
-    </div>
+                        <button type="submit" class="btn">Submit</button>
 
-    <div class="form-group">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
+                        <div class="inputBx-input">
+                            <p>Have An Account? <a href="./index.html">Login</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
     </div>
-
-    <div class="form-group">
-        <label for="no_telp" class="form-label">Nomer Telephone</label>
-        <input type="number" class="form-control" id="no_telp" placeholder="Nomer Telepon" name="no_telp" required>
-    </div>
-
-    <div class="form-group">
-        <label for="password" class="form-label">password</label>
-        <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
-    </div>
-    </div>
-    <div class="modal-footer">
-        <button type="submit" class="btn btn-primary">Registrasi</button>
-
-    </div>
-</form>
