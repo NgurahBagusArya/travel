d34a67b8f59a642ced18ffa6598e103aee1882e1
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 10:05:40 +0800

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,77 @@
-<br><br><br><br>
-<h1>Selamat Datang</h1>
-<p>Halo Saya</p>
+<header>
+    <div class="banner">
+        <link rel="stylesheet" href="<?= BASEURL; ?>/css/style-index.css">
+        <div class="container">
+            <h1 class="banner-title">
+                <span>EXPLORE</span>
+            </h1>
+            <h1>NUSA PENIDA WITH US</h1>
+            <p>1 Day Trip in nusa penida (Diamond Beach, Broken Beach, Blue Lagoon, Angel Beach)</p>
+            <form>
+                <input type="text" class="search-input" placeholder="Find your destinations...">
+                <button type="submit" class="search-btn">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+        </div>
+    </div>
+</header>
+<section class="design" id="design">
+    <div class="container">
+        <div class="title">
+            <h2>Why Book With TravelKuy?</h2>
+        </div>
+        <div class="design-content d-flex flex-wrap">
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/Garuda Wisnu Kencana.jpg" alt="" class="img-fluid">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Garuda Wisnu Kencana</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Garuda Wisnu Kencana adalah destinasi dari Badung</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/Kelingking Beach.jpg" alt="" class="img-fluid">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Kelingking Beach</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Pantai Kelingking adalah destinasi dari Nusa Penida</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/Luxury Bali Holidays.jpg" alt="" class="img-fluid">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Luxury Bali</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Bali Luxury adalah destinasi dari Tabanan</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/Pantai Melasti.jpg" alt="" class="img-fluid">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Melasti Beach</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Pantai Melasti adalah destinasi dari Badung</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/penglipuran.jpg" alt="" class="img-fluid">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Desa Penglipuran</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Desa Penglipuran adalah destinasi dari Karangasem</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
