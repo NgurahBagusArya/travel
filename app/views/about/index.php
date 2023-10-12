224e8c0dc0589ea985199ce0a05f523415a70069
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 11:49:53 +0800

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,10 +1,17 @@
-<div class="banner">
-    <img src="img/main.jpg" alt="" width="100%">
-</div>
-    <div class="about">
-    <h1>Index (About)</h1>
-    <p>Halo, I Am Leo I Am Gamer</p>
-    <p>Halo, i Am <?= $data['nama'];  ?> i Am <?= $data['pekerjaan']; ?></p>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style-about.css">
+    <br><br><br>
+    <div class="section">
+        <div class="container">
+            <div class="title">
+                <h1>About</h1>
+            </div>
+            <div class="contents">
+                <div class="text">
+                    <h3>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h3>
+                </div>
+            </div>
+            <div class="img">
+                <img src="<?= BASEURL ?>/img/GWK.png">
+            </div>
+        </div>
     </div>
-
-  
