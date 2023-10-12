c08e4655c422ed880b70175933b9bbb0b4e0d07e
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 11:49:55 +0800

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -24,7 +24,7 @@
         <div class="design-content d-flex flex-wrap">
             <div class="design-item">
                 <div class="design-img">
-                    <img src="<?= BASEURL ?>/img/Garuda Wisnu Kencana.jpg" alt="" class="img-fluid">
+                    <img src="<?= BASEURL ?>/img/Garuda Wisnu Kencana.jpg" alt="" class="img-fluid" height="400px">
                     <span><i class="far fa-heart"></i> 22</span>
                     <span>Garuda Wisnu Kencana</span>
                 </div>
@@ -34,7 +34,7 @@
             </div>
             <div class="design-item">
                 <div class="design-img">
-                    <img src="<?= BASEURL ?>/img/Kelingking Beach.jpg" alt="" class="img-fluid">
+                    <img src="<?= BASEURL ?>/img/Kelingking Beach.jpg" alt="" class="img-fluid" height="400px">
                     <span><i class="far fa-heart"></i> 22</span>
                     <span>Kelingking Beach</span>
                 </div>
@@ -44,7 +44,7 @@
             </div>
             <div class="design-item">
                 <div class="design-img">
-                    <img src="<?= BASEURL ?>/img/Luxury Bali Holidays.jpg" alt="" class="img-fluid">
+                    <img src="<?= BASEURL ?>/img/Luxury Bali Holidays.jpg" alt="" class="img-fluid" height="400px">
                     <span><i class="far fa-heart"></i> 22</span>
                     <span>Luxury Bali</span>
                 </div>
@@ -54,7 +54,7 @@
             </div>
             <div class="design-item">
                 <div class="design-img">
-                    <img src="<?= BASEURL ?>/img/Pantai Melasti.jpg" alt="" class="img-fluid">
+                    <img src="<?= BASEURL ?>/img/Pantai Melasti.jpg" alt="" class="img-fluid" height="400px">
                     <span><i class="far fa-heart"></i> 22</span>
                     <span>Melasti Beach</span>
                 </div>
@@ -64,7 +64,7 @@
             </div>
             <div class="design-item">
                 <div class="design-img">
-                    <img src="<?= BASEURL ?>/img/penglipuran.jpg" alt="" class="img-fluid">
+                    <img src="<?= BASEURL ?>/img/penglipuran.jpg" alt="" class="img-fluid" height="400px">
                     <span><i class="far fa-heart"></i> 22</span>
                     <span>Desa Penglipuran</span>
                 </div>
@@ -72,6 +72,17 @@
                     <a href="#">Desa Penglipuran adalah destinasi dari Karangasem</a>
                 </div>
             </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/tanahlottemple.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> 22</span>
+                    <span>Tanah Lot</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Tanah Lot adalah destinasi dari Tabanan</a>
+                </div>
+            </div>
         </div>
     </div>
 </section>
+
