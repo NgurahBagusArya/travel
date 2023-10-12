9b08b4936e678ddbd31ac73d8e01dca80ecadf17
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 11:49:57 +0800

    Update navbar.php

--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -9,6 +9,7 @@
           </div>
           <li><a href="<?= BASEURL ?>">Home</a></li>
           <li><a href="<?= BASEURL ?>/About">About</a></li>
+          <li><a href="<?= BASEURL ?>">Blog</a></li>
           <li><a href="<?= BASEURL ?>">Services</a></li>
           <li><a href="<?= BASEURL ?>">Contact</a></li>
           <li><a href="<?= BASEURL ?>/login">Login</a></li>
@@ -18,3 +19,5 @@
         </div>
       </div>
     </nav>
+
+    
