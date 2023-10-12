cd995717d53b89202bf596856e154178097c7867
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-12 12:38:15 +0800

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> <?= $data['judul']; ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/navbar.css">
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/style-login.css">
