ae8491724040c807afefe8cfcb291ff5ced6cae0
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-14 23:41:17 +0800

    Update registrasi.php

--- app/views/login/registrasi.php
+++ app/views/login/registrasi.php
@@ -1,40 +1,42 @@
-
-    
+<header>
     <div class="container d-flex justify-content-center">
-        <section>
-            <div class="imgBx">
-                <img src="<?= BASEURL ?>\img\background-register" alt="Background Image">
-            </div>
-            <div class="contentBx">
-                <div class="formBx">
-                    <br><br>
-                    <h2>Register</h2>
-                    <p>Welcome To TravelKuy</p>
-                    <form action="<?= BASEURL ?>/login/tambah" method="post">
-                        <div class="inputBx">
-                            <span></span>
-                            <input type="text" class="form-control" id="username" placeholder="Username" name="username" style="background: transparent;" required>
-                        </div>
-                        <div class="inputBx">
-                            <span></span>
-                            <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
-                        </div>
-                        <div class="inputBx">
-                            <span></span>
-                            <input type="number" class="form-control" id="no_telp" placeholder="Nomer Telepon" name="no_telp" required>
-                        </div>
-                        <div class="inputBx">
-                            <span></span>
-                            <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
-                        </div>
+        <section class="login-section">
+            <div class="center-register">
+                <div class="imgBx">
+                    <img src="<?= BASEURL ?>\img\background-register" alt="Background Image">
+                </div>
+                <div class="contentBx">
+                    <div class="formBx">
+                        <br><br>
+                        <h2>Register</h2>
+                        <p>Welcome To TravelKuy</p>
+                        <form action="<?= BASEURL ?>/login/tambah" method="post" class="form-login">
+                            <div class="inputBx">
+                                <span></span>
+                                <input type="text" class="form-control" id="username" placeholder="Username" name="username" style="background: transparent;" required>
+                            </div>
+                            <div class="inputBx">
+                                <span></span>
+                                <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
+                            </div>
+                            <div class="inputBx">
+                                <span></span>
+                                <input type="number" class="form-control" id="no_telp" placeholder="Nomer Telepon" name="no_telp" required>
+                            </div>
+                            <div class="inputBx">
+                                <span></span>
+                                <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+                            </div>
 
-                        <button type="submit" class="btn">Submit</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
 
-                        <div class="inputBx-input">
-                            <p>Have An Account? <a href="./index.html">Login</a></p>
-                        </div>
-                    </form>
+                            <div class="inputBx-input">
+                                <p>Have An Account? <a href="./index.html">Login</a></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </section>
     </div>
+</header>
