f525a7d17b81813acc6d2c6ae5a082fe4ab2c158
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-14 23:41:16 +0800

    Update login.php

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,29 +1,27 @@
 <div class="hero"></div>
 <div class="content"></div>
-
-<section class="background">
-    <div class="background-img">
-    <img src="<?= BASEURL ?>/img/hero.jpg" alt="" height="">
-    <div class="center">
-        <h1>Login</h1>
-        <P>Welcome Back To TravelKuy</P>
-        <?php if (isset($pesan)) : ?>
-            <div class="alert alert-danger">
-                <?php echo $pesan; ?>
-            </div>
-        <?php endif; ?>
-        <form action="<?= BASEURL; ?>/login" method="post">
-            <div class="txt_field">
-                <input type="email" class="form-control" placeholder="Email" id="email" name="email" required>
-                <span></span>
-            </div>
-            <div class="txt_field">
-                <input type="password" class="form-control" placeholder="Password" id="password" name="password" required>
-                <span></span>
-            </div>
-            <div class="pass">Forgot Password? <a href="<?= BASEURL ?>/login/registrasi">Register</a></div>
-            <button type="submit" name="login" class="btn btn-primary">Login</button>
-        </form>
-        <br>
+<header class="login-background">
+    <section class="login-section">
+        <div class="center">
+            <h1>LOGIN</h1>
+            <P>Welcome Back To TravelKuy</P>
+            <?php if (isset($pesan)) : ?>
+                <div class="alert alert-danger">
+                    <?php echo $pesan; ?>
+                </div>
+            <?php endif; ?>
+            <form action="<?= BASEURL; ?>/login" method="post">
+                <div class="txt_field">
+                    <input type="email" class="form-control" placeholder="Email" id="email" name="email" required>
+                    <span></span>
+                </div>
+                <div class="txt_field">
+                    <input type="password" class="form-control" placeholder="Password" id="password" name="password" required>
+                    <span></span>
+                </div>
+                <div class="pass">Forgot Password? <a href="<?= BASEURL ?>/login/registrasi">Register</a></div>
+                <button type="submit" name="login" class="btn btn-primary">Login</button>
+            </form>
+            <br>
     </section>
-</div>
+</header>
