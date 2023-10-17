70d78db4520126b7bf3ae998d33c12d33d28e8c2
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-17 10:31:54 +0800

    Create readmore.php

---
+++ app/views/blog/readmore.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4 title "><?= $row['judul']?></h1>
+    <p class="lead"><? $row{'konten'}?></p>
+  </div>
+</div>
