b1668549e564cebd11fb0e98effef2c82491fe0e
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-18 08:40:36 +0800

    Update blog.php

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -3,9 +3,10 @@
 class blog extends Controller{
     public function index()
     {
-        $data['judul'] = 'Daftar Blog';
+        $data['judul'] = 'Blog - TravelKuy';
         $data['blog'] = $this->model('Blog_model')->getAlltraveldata();
         $this -> view ('Templates/header' ,$data);
+        $this->view('Templates/navbar', $data);
         $this -> view ('blog/index', $data);
         $this -> view ('Templates/footer');
     }
@@ -16,6 +17,7 @@ class blog extends Controller{
         $data['judul'] = 'Read More';
         $data['blog'] = $this->model('Blog_model')->getblogById($id_blog);
         $this -> view ('Templates/header' ,$data);
+        $this->view('Templates/admin-navbar', $data);
         $this -> view ('blog/readmore', $data);
         $this -> view ('Templates/footer');
     }
