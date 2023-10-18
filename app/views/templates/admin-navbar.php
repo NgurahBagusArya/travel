da7c852e0f50d06561aca393536379dc70d20285
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-18 09:28:27 +0800

    Update admin-navbar.php

--- app/views/templates/admin-navbar.php
+++ app/views/templates/admin-navbar.php
@@ -51,7 +51,7 @@
                     </li>
                     <li>
                         <a href='<?= BASEURL ?>/admin/blog'>
-                            <i class='bi bi-person'></i>
+                            <i class="bi bi-newspaper"></i>
                             <span class='links_name'>Blog</span>
                         </a>
                     </li>
