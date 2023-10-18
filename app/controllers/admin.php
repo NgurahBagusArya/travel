457098d5eba477cb961a0351b9d4768c7e5ea59f
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-18 08:08:43 +0800

    Update admin.php

---
+++ app/controllers/admin.php
@@ -0,0 +1,62 @@
+<?php 
+class admin extends Controller{
+    public function index()
+    { 
+            $data['judul'] = 'Blog Admin';
+            $this->view('Templates/admin-header', $data);
+            $this->view('Templates/admin-navbar', $data);
+            $this->view('admin/index', $data);
+            $this->view('Templates/footer');
+
+    }
+
+    public function blog() {
+            $data['judul'] = 'Blog Admin';
+            $data['blog'] = $this->model('Blog_model')->getAlltraveldata();
+            $this->view('Templates/admin-header', $data);
+            $this->view('Templates/admin-navbar', $data);
+            $this->view('admin/blog', $data);
+            $this->view('Templates/footer');
+    }
+
+    public function readmore($id_blog)
+    {
+        var_dump($id_blog);
+        $data['judul'] = 'Read More';
+        $data['blog'] = $this->model('Blog_model')->getblogById($id_blog);
+        $this -> view ('Templates/header' ,$data);
+        $this -> view ('blog/readmore', $data);
+        $this -> view ('Templates/footer');
+    }
+    
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $judul = $_POST['judul'];
+            $author = $_POST['author'];
+            $konten = $_POST['konten'];
+            
+            $data = [
+                'judul' => $judul,
+                'author' => $author,
+                'konten' => $konten
+            ];
+    
+            if ($this->model('Blog_model')->tambahkanblog($data) > 0) {
+                 header('Location: ' . BASEURL . '/admin/blog');
+                 exit;
+            }
+        }
+    }
+    public function delete($id)
+    {
+        if ($this->model('Admin_model')->hapusblog($id) > 0) {
+            header('Location: ' . BASEURL . '/admin/blog');
+            exit;
+        }
+    }
+    
+
+}
+
+?>
