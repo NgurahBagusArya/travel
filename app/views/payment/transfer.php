c62f1114a0742817054df359da2d138c184e7454
shiromikuma <110903973+shiromikuma@users.noreply.github.com>
2023-10-19 09:05:50 +0800

    Create transfer.php

---
+++ app/views/payment/transfer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>transfer</title>
+    <link rel="stylesheet" href="./transfer.css">
+</head>
+<body>
+    <div class="view">
+        <div class="kr">
+            <div class="logo"><img src="../img/Logo BCA_Biru.png" alt=""></div>
+            <div class="norek">
+                <p class="textno">No. Rekening :</p>
+                <p class="no">002 89667 673557</p>
+            </div>
+            <div class="tutor">
+                <div class="jdl">
+                    <p>Tatacara Transfer Bank :</p>
+                </div>
+                <div class="tutor-a">
+                    <p class="headline">1. Buka m-banking </p>
+                    <p class="desk">buka aplikasi BCA mobile</p>
+                </div>
+                <div class="tutor-b">
+                    <p class="headline">2. Pilih menu m-transfer</p>
+                    <p class="desk">pilih menu m-transfer lalu masuk ke kalaman menu Transfer antar bank</p>
+                </div>
+                <div class="tutor-c">
+                    <p class="headline">3. Masukan nomor rekening tujuan</p>
+                    <p class="desk">masukan nomor rekening tujuan (nomor di atas) serta masukan jumlah tagihan</p>
+                </div>
+                <div class="tutor-d">
+                    <p class="headline">4. Masukan PIN</p>
+                    <p class="desk">masukan PIN m-BCA anda lalu tekan kirim</p>
+                </div>
+            </div>
+        </div>
+        <div class="kn">
+            <p class="sisawaktu">Sisa waktu untuk melakukan transfer</p>
+            <div id="countdown"></div>
+           
+        </div>
+    </div>
+    <script src="./time.js"></script>
+</body>
+</html>
