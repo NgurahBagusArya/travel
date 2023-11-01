df4050dc96342e875f70dd6d5043fa553e5ffa6b
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-11-01 11:21:47 +0800

    a

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,7 +1,37 @@
 <?php
-class Login extends Controller {
-    public function index() {
+class Login extends Controller
+{
+    public function index()
+    {
+        session_start(); // Mulai sesi di sini
+
+        if (isset($_SESSION['user_id'])) {
+            // Jika pengguna sudah login, alihkan ke halaman beranda atau halaman lain yang sesuai
+            if ($_SESSION['level'] === 'superadmin') {
+                header('Location: ' . BASEURL . '/Superadmin');
+            } elseif ($_SESSION['level'] === 'admin') {
+                header('Location: ' . BASEURL . '/admin');
+            } else {
+                header('Location: ' . BASEURL);
+            }
+            exit;
+        }
+
         if (isset($_POST['login'])) {
+            session_start(); // Mulai sesi di sini
+
+            if (isset($_SESSION['user_id'])) {
+                // Jika pengguna sudah login, alihkan ke halaman beranda atau halaman lain yang sesuai
+                if ($_SESSION['level'] === 'superadmin') {
+                    header('Location: ' . BASEURL . '/Superadmin');
+                } elseif ($_SESSION['level'] === 'admin') {
+                    header('Location: ' . BASEURL . '/admin');
+                } else {
+                    header('Location: ' . BASEURL);
+                }
+                exit;
+            }
+
             $this->prosesLogin();
         } else {
             $data['judul'] = 'Login';
@@ -27,14 +57,29 @@ class Login extends Controller {
         }
     }
 
-    public function prosesLogin() {
+    public function Authuser()
+    {
+        if (isset($_SESSION['user_id'])) {
+            if ($_SESSION['level'] === 'admin') {
+                header('Location: ' . BASEURL . '/admin'); // Pengguna dengan peran admin
+            } elseif ($_SESSION['level'] === 'superadmin') {
+                header('Location: ' . BASEURL . '/superadmin'); // Pengguna dengan peran super admin
+            } else {
+                header('Location: ' . BASEURL); // Pengguna lain
+            }
+            exit;
+        }
+    }
+
+    public function prosesLogin()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $email = $_POST['email'];
             $password = $_POST['password'];
-    
+
             $userModel = $this->model('Users_Model');
             $user = $userModel->getUserByEmail($email);
-    
+
             if ($user) {
                 if (password_verify($password, $user['password'])) {
                     session_start(); // Mulai sesi di sini
@@ -42,13 +87,13 @@ class Login extends Controller {
                     $_SESSION['email'] = $user['email'];
                     $_SESSION['username'] = $user['username'];
                     $_SESSION['level'] = $user['level'];
-    
+
                     if ($user['level'] === 'admin') {
                         header('Location: ' . BASEURL . '/admin');
                     } else {
                         header('Location: ' . BASEURL);
                     }
-    
+
                     exit;
                 } else {
                     $pesan = 'Password salah.';
@@ -60,23 +105,23 @@ class Login extends Controller {
             }
         }
     }
-    
-    
-    public function prosesLogout(){
-    session_start();
-    session_destroy();
 
-    header('Location: ' . BASEURL . '/login');
-    exit();
+
+    public function prosesLogout()
+    {
+        session_start();
+        session_destroy();
+
+        header('Location: ' . BASEURL . '/login');
+        exit();
     }
 
-    private function tampilkanPesanError($pesan) {
+    private function tampilkanPesanError($pesan)
+    {
         $data['pesan'] = $pesan;
         $data['judul'] = 'Login';
         $this->view('templates/header', $data);
         $this->view('login/login', $data);
         $this->view('templates/footer');
     }
-
 }
-
--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -8,7 +8,7 @@
         <i class="fas fa-times"></i>
       </div>
       <li><a href="<?= BASEURL ?>" class="nav-list">Home</a></li>
-      <li><a href="<?= BASEURL ?>/About"  class="nav-list">About</a></li>
+      <li><a href="<?= BASEURL ?>/About" class="nav-list">About</a></li>
       <li>
         <div class="dropdown show">
           <a class="dropdown-toggle nav-list" href="<?= BASEURL ?>/services" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -21,8 +21,19 @@
           </div>
         </div>
       </li>
-      <li><a href="<?= BASEURL ?>/contact"  class="nav-list">Contact</a></li>
-      <li><a href="<?= BASEURL ?>/login"  class="nav-list">Login</a></li>
+      <li><a href="<?= BASEURL ?>/contact" class="nav-list">Contact</a></li>
+      <li>
+        <?php
+        session_start();
+        if (isset($_SESSION['user_id'])) {
+          // Pengguna sudah login
+          echo '<a href="' . BASEURL . '/login/prosesLogout" class="nav-list">Logout</a>';
+        } else {
+          // Pengguna belum login
+          echo '<a href="' . BASEURL . '/login" class="nav-list">Login</a>';
+        }
+        ?>
+      </li>
     </ul>
     <div class="icon menu-btn">
       <i class="fas fa-bars"></i>
