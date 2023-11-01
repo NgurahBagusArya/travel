2f6d3a9eb0e11709a2b574132cf1dc7b606f608d
Adiprana <112458353+Adiprana@users.noreply.github.com>
2023-11-01 08:43:11 +0800

    readblog

--- app/views/blog/readmore.php
+++ app/views/blog/readmore.php
@@ -1,20 +1,20 @@
-<div class="jumbotron jumbotron-fluid">
+<!-- <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4 title "><?= $row['judul']?></h1>
     <p class="lead"><? $row['konten']?></p>
   </div>  
-</div>
+</div> -->
 
 <br></br>
-  <div class="container">
+  <div class="container" style="margin-top: 100px;">
   <!-- Stack the columns on mobile by making one full-width and the other half-width -->
   <div class="row">
     <div class="col-md-8">
-      <h1 class="italic-font">Tahukah Anda Kelingking Beach Dimana?</h1>
+      <h1 class="italic-font" style="font-style: italic;">Tahukah Anda Kelingking Beach Dimana?</h1>
       <p class="card-text"><small class="text-muted">Blog Ini Sepenuhnya Dikerjakan Oleh Tim TravelKuy</small></p>
       <!-- <p class="card-text"><small class="text-muted">Terakhir diupdate 1 menit yang lalu oleh tim TravelKuy</small></p> -->
         <div class="card mb-3">
-          <img src="./imgae/Kelingking Beach.jpg" class="card-img-top" alt="...">
+          <img src="<?= BASEURL ?>/img/Kelingking-Beach.jpg" alt="image 1" class="img-fluid">
           <div class="card-body">
             <h5 class="card-title">Kelingking Beach</h5>
             <p class="card-text" >Nama <span class="text-style">Kelingking Beach</span> nampaknya masih asing di kalangan masyarakat kita dan belum banyak diketahui padahal tempat ini menyimpan keindahan yang sangat menakjubkan. Bila ada yang belum tahu Kelingking Beach dimana, Jawabannya adalah di 3 nusa daerah Bali. Lebih tepatnya lokasi Kelingking Beach dimana terletak di Nusa Penida Bali yaitu pulau kecil yang berada di dekat pulau Bali. Kelingking Beach Nusa Penida Bali adalah destinasi wisata pantai yang menyajikan keelokan alam yang akan membuat Anda merasa kagum atas alam yang diciptakan oleh Tuhan. Walaupun hanya bisa menikmati pantai ini dari atas tebing karena ada akses jalan yang susah untuk menuju ke bawah, hal ini tidak mengurangi rasa takjub Anda. Pantainya begitu tenang dengan pasir putih, karang, tebing, gua kapur dan karst di tepi laut.</p>
@@ -22,9 +22,9 @@
         </div>
       
       <div class="card mb-3">
-          <img src="https://cdn.pixabay.com/photo/2014/08/11/11/50/moon-415501_1280.jpg" class="card-img-top" alt="...">
+          <img src="<?= BASEURL ?>/img/kelingking-beach-manta-spot.jpg" alt="diving-manta-spot" class="img-fluid">
           <div class="card-body">
-            <h5 class="card-title">Spot Diving Manta</h5>
+            <h5 class="card-title" style="font-style: italic;">Spot Diving Manta</h5>
             <p class="card-text">
                 Kelingking Beach adalah tempat yang memiliki spot yang cukup dikenal untuk diving karena menyajikan pemandangan yang sangat langka. Spot ini dinamakan sebagai Manta Point. Sejarah Kelingking Beach Manta Point ini dikenal karena adanya ikan Manta (ikan pari paling besar sedunia). Para menyelam dapat berenang bebas di sini bersama dengan ikan Manta yang sudah langka. Hal ini pasti akan menjadi pengalaman yang menyenangkan dan tidak terlupakan.
                 <br>
@@ -39,9 +39,9 @@
         </div>
       
       <div class="card mb-3">
-          <img src="https://cdn.pixabay.com/photo/2016/05/01/21/20/earth-1365995_1280.jpg" class="card-img-top" alt="...">
+          <img src="<?= BASEURL ?>/img/lokasi-kelingking-beach.jpg" alt="diving-manta-spot" class="img-fluid">
           <div class="card-body">
-            <h5 class="card-title">Lokasi Kelingking Beach</h5>
+            <h5 class="card-title" style="font-style: italic;">Lokasi Kelingking Beach</h5>
             <p class="card-text">Lokasi Kelingking Beach berada di Dusun Karang Dawa, Desa Bunga Mekar, Kecamatan Nusa Penida. Cara menuju Kelingking Beach harus menggunakan transportasi air.
               Karena Nusa Penida merupakan pulau sendiri jadi kamu harus menggunakan transportasi laut untuk sampai ke Pulau ini. Kalau kamu berangkat dari Pantai Sanur kamu bisa menggunakan Perahu Umum atau sering disebut dengan Jukung, dan cukup membayar Rp. 40.000 dengan waktu tempuh perjalanan 1,5 jam 
 
@@ -59,7 +59,7 @@ Infrastruktur yang ada di Nusa Penida belum sepenuhnya jadi, dan juga transporta
         <div class="card-body">
           <h5>Kelingking Beach</h5>
           <hr/>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.378823694858!2d115.46800837136544!3d-8.750381170060596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd26f1616cd2cc7%3A0xee84b7df0afbff19!2sKelingking%20Beach%20Nusa%20Penida!5e0!3m2!1sid!2sus!4v1698205408241!5m2!1sid!2sus" width="322" height="350" style="border: 0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.378823694858!2d115.46800837136544!3d-8.750381170060596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd26f1616cd2cc7%3A0xee84b7df0afbff19!2sKelingking%20Beach%20Nusa%20Penida!5e0!3m2!1sid!2sus!4v1698205408241!5m2!1sid!2sus" width="362" height="350" style="border: 0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
        <br/>
@@ -84,7 +84,7 @@ Infrastruktur yang ada di Nusa Penida belum sepenuhnya jadi, dan juga transporta
           <h5>Kelingking Beach View</h5>
           <hr/>
           <div class="ratio ratio-16x9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/_20p_iEbmdU?si=lw6mc_5Wx4O-yyCb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="366" height="200" src="https://www.youtube.com/embed/_20p_iEbmdU?si=lw6mc_5Wx4O-yyCb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
         </div>
       </div>
