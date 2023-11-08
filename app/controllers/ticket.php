209c9a015bfe02a6674b9bb779183f2c4d68e29f
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-11-08 10:21:04 +0800

    a

--- app/controllers/ticket.php
+++ app/controllers/ticket.php
@@ -3,6 +3,7 @@ class ticket extends Controller{
     public function index()
     { 
             $data['judul'] = 'Ticket - TravelKuy';
+            $data['trip'] = $this->model('Trip_model')->getAlltripdata();
             $this->view('Templates/header', $data);
             $this->view('Templates/navbar', $data);
             $this->view('ticket/index', $data);
--- app/views/ticket/index.php
+++ app/views/ticket/index.php
@@ -1,5 +1,17 @@
-<div class="home-content">
+<div class="user-content">
     <div class="container">
-        
+        <br><br><br><br><br>
+        <?php foreach ($data['trip'] as $row) : ?>
+            <div class="col-md-4 col-sm-6 col-12 mb-4 text_right">
+                <div class="ticket">
+                    <img src="http://localhost<?= $row['image']; ?>" class="card-img-top" alt="Image">
+                    <div class="isi">
+                        <div class="judul"><?= $row['nama_trip']; ?></div>
+                        <div class="harga">Rp.<?= $row['harga']; ?>,-</div>
+                        <div class="slot">Sisa Tiket <?= $row['slot_tiket']; ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            </div>
     </div>
 </div>
